1c8eae2c53dab342a4369fa3ebd2ec3d58fa7e50
koskine6 <esa.t.koskinen@aalto.fi>
2016-11-21 18:31:59 +0200

    Added some framework for launcher and tests.

---
+++ src/Tests.cpp
@@ -0,0 +1,31 @@
+int runTests() {
+    int testsPassed = 0;
+    
+    if (runNetworkTest() != 0) {
+        std::cout << "Network test failed! Terminating tests." << std::endl;
+        return -1;
+    } else {
+        testsPassed += 1;
+        std::cout << "Network test passed." << std::endl;
+    }
+    
+    if (runPhysicsTest() != 0) {
+        std::cout << "Physics test failed! Terminating tests." << std::endl;
+        return -2;
+    } else {
+        testsPassed += 1;
+        std::cout << "Physics test passed." << std::endl;
+    }
+    
+    std::cout << std::endl << "All tests were OK." << std::endl << testsPassed << " tests were passed." std::endl;
+    return 0;
+}
+
+int runNetworkTest() {
+    Node testNode ();
+    return 0;
+}
+
+int runPhysicsTest() {
+    return 0;
+}

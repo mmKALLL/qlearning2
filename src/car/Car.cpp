a2df708041b2149e4b0154ceca56f5216bf77769
Olli Kauppinen <kauppio3@aalto.fi>
2016-11-24 21:38:11 +0200

    Added basic car functionality to go forward and turn

--- src/car/Car.cpp
+++ src/car/Car.cpp
@@ -12,6 +12,7 @@ Car::Car(b2World * world, const b2Vec2& position, const b2Vec2& dimensions)
 	carBodyDef.position.Set(position.x, position.y);
 	carBodyDef.angle = 0; //set the starting angle
 	carBody = world->CreateBody(&carBodyDef);
+	carBody->SetAngularDamping(3);
 
 	b2PolygonShape carShape;
 	// Car is 4 meters long and 1.5 meter wide
@@ -25,6 +26,10 @@ Car::Car(b2World * world, const b2Vec2& position, const b2Vec2& dimensions)
 	carFixtureDef.restitution = 0.25;
 	// Well, friction
 	carFixtureDef.friction = 0.5;
+
+	//Add car to world
+	carBody = world->CreateBody(&carBodyDef);
+	carBody->CreateFixture(&carFixtureDef);
 }
 
 
@@ -34,16 +39,66 @@ Car::~Car()
 	
 }
 
-void Car::accelerate()
+void Car::accelerate(int direction)
 {
-	carBody->ApplyForce(b2Vec2(0, 50), carBody->GetWorldCenter(), true);
+	// Get current forward speed and set force 
+	b2Vec2 currentForwardNormal = getForwardVelocity();
+	force = 0;
+
+	// Set desired speed in relation to if we are reversing or going forwards
+	if (direction > 0) {
+		desiredSpeed = maxSpeed;
+	}
+	else {
+		desiredSpeed = maxReverse;
+	}
+
+
+	currentForwardNormal = carBody->GetWorldVector(b2Vec2(1, 0));
+
+	currentSpeed = b2Dot(getForwardVelocity(), currentForwardNormal);
+
+	//Depending on current speed the amount of force is determined
+	if (desiredSpeed > currentSpeed) {
+		force = maxDriveForce;
+	}
+	else if (desiredSpeed < currentSpeed) {
+		force = -maxReverseForce;
+	}
+	else { 
+		return; 
+	}
+
+	// Apply the force
+	if (direction > 0) {
+		carBody->ApplyForce(force * currentForwardNormal, carBody->GetWorldCenter(), false);
+	}
+	else {
+		carBody->ApplyForce(-force * currentForwardNormal, carBody->GetWorldCenter(), false);
+	}
+
 }
 
-void Car::brake()
+
+void Car::turn(int direction)
 {
-	carBody->ApplyForce(b2Vec2(0, 50), carBody->GetWorldCenter(), true);
+	if (direction > 0) {
+		carBody->ApplyTorque(250, true);
+	}
+	else {
+		carBody->ApplyTorque(-250, true);
+	}
 }
 
+b2Vec2 Car::getForwardVelocity() {
+	b2Vec2 currentRightNormal = carBody->GetWorldVector(b2Vec2(0, 1));
+	return b2Dot(currentRightNormal, carBody->GetLinearVelocity()) * currentRightNormal;
+}
+
+b2Vec2 Car::getLateralVelocity() {
+	b2Vec2 currentRightNormal = carBody->GetWorldVector(b2Vec2(1, 0));
+	return b2Dot(currentRightNormal, carBody->GetLinearVelocity()) * currentRightNormal;
+}
 
 
 Tire::Tire()
--- src/car/Car.hpp
+++ src/car/Car.hpp
@@ -4,15 +4,30 @@ class Car
 {
 
 public:
+	// Constructors
 	Car(b2World * world, const b2Vec2& position, const b2Vec2& dimensions);
 	~Car();
 
-	void accelerate();
-	void brake();
+	// Function definitions
+	void accelerate(int direction);
+	void turn(int direction);
+	b2Vec2 getForwardVelocity();
+	b2Vec2 getLateralVelocity();
+	
+	// Variables for desired speed, current speed, current force applied and maximun force that can be applied
+	float desiredSpeed = 0;
+	float currentSpeed = 0;
+	float force = 0;
+	
 	
 
 private:
+	// Variables for the body amd maximum speeds
 	b2Body* carBody = nullptr;
+	float maxSpeed = 100;
+	float maxReverse = 50;
+	float maxDriveForce = 500;
+	float maxReverseForce = 250;
 
 };
 

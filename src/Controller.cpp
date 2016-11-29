93f4f3d1439c9f22c8f1cc7164eec9a1eddb841b
koskine6 <esa.t.koskinen@aalto.fi>
2016-11-29 19:44:47 +0200

    Added more controller code, etc.

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -1,10 +1,27 @@
 #include "Controller.hpp"
+#include <vector>
 
-// FIXME: This is here for the time being to have the project compile
-int main() {
-	return 0;
+Controller::Controller() {
+	// TODO: Esa: Implement constructor
+}
+
+const std::vector<float> getSightVector(int size, int degrees) const {
+	// TODO: Olli/Jussi: Distances to walls in various directions, spanning a vision width of specified degrees.
+}
+
+const float getCarDistanceFromMiddle() const {
+	// TODO: Jussi: Implement function to get car middle point's distance from center of track
+}
+
+const float getCarSpeed() const {
+	// TODO: Olli/Jussi: Return the car's current speed.
+}
+
+const vector<float> getCarPosition() const {
+	// TODO: Olli/Jussi: Return the car's current x/y position as two-element vector.
 }
 
 void Controller::stepForward() {
-
+	// TODO: Esa: implement the main logic
 }
+
